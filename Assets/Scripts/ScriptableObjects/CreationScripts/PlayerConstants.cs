cf6db3f206329458a7b378bb050d6eec8718da23
sihvn <77093664+sihvn@users.noreply.github.com>
2023-11-05 22:32:03 +0800

    Added Stress Bar

--- Assets/Scripts/ScriptableObjects/CreationScripts/PlayerConstants.cs
+++ Assets/Scripts/ScriptableObjects/CreationScripts/PlayerConstants.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName =  "PlayerConstants", menuName =  "ScriptableObjects/PlayerConstants", order =  1)]
-public  class PlayerConstants : ScriptableObject
+[CreateAssetMenu(fileName = "PlayerConstants", menuName = "ScriptableObjects/PlayerConstants", order = 1)]
+public class PlayerConstants : ScriptableObject
 {
     public int moveSpeed;
     public int maxMoveSpeed;
-    
+    public int stressPoint;
+    public int maxStressPoint;
+    public int sanityPoint;
+    public int performancePoint;
+
 
 }
