8a62bc1f70c11168c5838fbfa25cc4dc56a6b657
wangsiyang68 <siyang.official@gmail.com>
2023-11-09 08:46:11 +0800

    feat: Add basic Pause UI
    Only Resume Button working so far

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isActive;
+    void Start()
+    {
+        isActive = false;
+        gameObject.SetActive(isActive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleOnState()
+    {
+        isActive = !isActive;
+        gameObject.SetActive(isActive);
+    }
+}
