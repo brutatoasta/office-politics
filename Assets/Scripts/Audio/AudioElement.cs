ed8e40f5934ca5281e695c58b51285a2204660b3
eharnlee <eharnlee@gmail.com>
2023-11-13 18:26:08 +0800

    Add AudioElement struct and AudioType enum

---
+++ Assets/Scripts/Audio/AudioElement.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+[Serializable] // note: "System.Serializable" allows for the fields to be set via Unity Inspector
+public struct AudioElement
+{
+    /* Description:
+    This struct is used in each AudioElementGameEvent (scriptable object event) to pass information to the
+    AudioManager about which AudioType and AudioClip needs to be played.
+
+    This struct is also used in the AudioElements scriptable object, which stores all the audio elements used in the
+    game.
+    */
+
+    public AudioType audioType;
+    public AudioClip audioClip;
+}
