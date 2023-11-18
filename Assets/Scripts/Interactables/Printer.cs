eed1f441f357d5c3332aeb5c67686ee7faa87162
j-ntw <60847718+j-ntw@users.noreply.github.com>
2023-11-18 22:37:11 +0800

    add printer script

--- Assets/Scripts/Interactables/Printer.cs
+++ Assets/Scripts/Interactables/Printer.cs
@@ -1,46 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 // Takes and handles input and movement for a player character
-public class Printer : Receivable
+public class Printer : BaseInteractable
 {
-    public bool isScanning = false;
-    // public new void OnInteract(SpriteRenderer heldSprite)
-    // {
-    //     // called when player presses interact key
-    //     taskConstants.currentInput = validInput; // TODO: why is this line co opting what we put into the receivable via the editor?
-    //     SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-    //     if (GameManager.instance.held != null)
-    //     {
-    //         // maybe check other conditions
-    //         // drop the item in!
-    //         GameObject held = GameManager.instance.held;
-    //         GameManager.instance.held = null;
-    //         heldSprite.sprite = null;
+    public TaskName[] invalidInputs;
+    public TaskName[] validInputs;
+    private HashSet<TaskName> _invalidInputs;
+    private HashSet<TaskName> _validInputs; // hashset for faster checks
+    TaskName heldType;
+    GameObject held;
 
-    //         Debug.Log($"Dropped {held.name} into me!");
+    // FetchDoc
+    private bool isDocumentReady = false;
+    private GameObject fetchDoc; // placed outside map
+    private Sprite fetchDocSprite;
+    private float cookTime;
+    new void Awake()
+    {
+        base.Awake();
 
-    //         // calculate score
-    //         if (validInput != held.GetComponent<Holdable>().taskName)
-    //         {
-    //             // decrease score
-    //             playerConstants.performancePoints -= 5;
-    //             Debug.Log("decrease score");
-    //             animator.SetTrigger("doFlinch");
-    //         }
-    //         else
-    //         {
-    //             playerConstants.performancePoints += 5;
-    //             Debug.Log("increase score");
-    //             // animate for some time
-    //             animator.SetBool("isWiggling", true);
+        _validInputs = new HashSet<TaskName>(validInputs); // TODO: no need multiple inputs
+        _invalidInputs = new HashSet<TaskName>(invalidInputs);
+        fetchDocSprite = fetchDoc.GetComponent<SpriteRenderer>().sprite;
+    }
+    protected override bool CanInteract()
+    {
+        // you can only interact with printer when your hand has a PrintDoc or your hand is empty 
+        // but the printer has a to fetch doc
+        held = GameManager.instance.held;
+        if (held == null) // nothing in hand
+        {
+            return isDocumentReady;
+        }
+        else
+        {
+            heldType = held.GetComponent<Holdable>().taskName;
+            return _validInputs.Contains(heldType);
+        }
 
-    //             GameManager.instance.switchTasks.Invoke();
-    //         }
+    }
 
-    //     }
+    protected override void OnInteract()
+    {
+        // fetching doc
+        if (GameManager.instance.held == null)
+        {
 
-    // }
+            GameManager.instance.held = fetchDoc;
+            playerHand.sprite = fetchDocSprite;
+            Debug.Log($"Fetched {held.name} from printer!");
+            isDocumentReady = false;
+        }
+        else
+        {
+            // depositing PrintDoc
+            GameManager.instance.held = null;
+            playerHand.sprite = null;
+
+            Debug.Log($"Dropped {held.name} into printer!");
+            animator.SetTrigger("doWiggle");
+
+            // cook the doc
+            Cook();
+        }
+    }
+    private void Cook()
+    {
+        // coroutine changes isDocumentReady from false to true after x seconds
+    }
 }
