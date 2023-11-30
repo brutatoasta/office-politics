347dc68e9d16280858d07f21cfbe533b62c763b7
sihvn <77093664+sihvn@users.noreply.github.com>
2023-11-30 23:08:21 +0800

    Fixed minor details on the levels

--- Assets/Scripts/ScriptableObjects/CreationScripts/TaskConstants.cs
+++ Assets/Scripts/ScriptableObjects/CreationScripts/TaskConstants.cs
@@ -14,24 +14,14 @@ public class TaskConstants : ScriptableObject
             
             new TaskItem[]
             {
-                new(1, 1, 5, TaskName.FetchTea, "Fetch tea for Ann"),
-                new(1, 1, 5, TaskName.FetchCoffee, "Fetch coffee for Mary"),
-
-                // new(1, 1, 5, TaskName.PrepRefreshment, "Prepare refreshment in the meeting room"),
-                // new(1, 1, 5, TaskName.PrepMeeting, "Prepare Meeting Materials in the meeting room"),
-                // new(1, 1, 5, TaskName.Shred, "Shred documents"),
-                // new(1, 1,  5, TaskName.Laminate, "Laminate documents"),
-                //new(1, 1,  5, TaskName.FetchDoc, "Fetch documents from Susan for photocopy"),
-                //new(1, 1,5, TaskName.DeliverDoc, "Deliver photocopied documents to Jane"),
-
-                // new(1, 1,5, TaskName.RefillCoffee, "Refill the empty coffee pot"),
-
+                new(1, 1, 5, TaskName.FetchTea, "Fetch tea for Siyang"),
+                new(1, 1, 5, TaskName.FetchCoffee, "Fetch coffee for Joshua"),
             },
             // level 2
             new TaskItem[]
             {
-                new(1, 1, 5, TaskName.FetchTea, "Fetch tea for Ann"),
-                new(1, 1, 5, TaskName.FetchCoffee, "Fetch coffee for Mary"),
+                new(1, 1, 5, TaskName.FetchTea, "Fetch tea for Natthan"),
+                new(1, 1, 5, TaskName.FetchCoffee, "Fetch coffee for Emmanuel"),
                 new(2, 1, 5, TaskName.PrepRefreshment, "Prepare refreshment in the meeting room"),
                 new(3, 1, 5, TaskName.PrepMeeting, "Prepare Meeting Materials in the meeting room"),
 
@@ -39,20 +29,20 @@ public class TaskConstants : ScriptableObject
             // level 3
             new TaskItem[]
             {
-                new(1, 1, 5, TaskName.Shred, "Shred documents"),
-                new(1, 1, 5, TaskName.Laminate, "Laminate documents"),
-                new(1, 1, 5, TaskName.FetchDoc, "Fetch documents from Susan for photocopy"),
-                new(1, 1, 5, TaskName.DeliverDoc, "Deliver photocopied documents to Jane"),
+                new(1, 1, 5, TaskName.Shred, "Shred documents in the BOSS room"),
+                new(1, 1, 5, TaskName.Laminate, "Laminate documents in the BOSS room"),
+                new(1, 1, 5, TaskName.FetchDoc, "Fetch documents from Jolin for photocopy"),
+                new(1, 1, 5, TaskName.DeliverDoc, "Deliver photocopied documents back to Jolin"),
             },
             // level 4
             new TaskItem[]
             {
-                new(1, 1,5, TaskName.Shred, "Shred documents"),
-                new(1, 1,5, TaskName.Laminate, "Laminate documents"),
-                new(3, 1, 5, TaskName.PrepRefreshment, "Prepare refreshment in the meeting room"),
-                new(2, 1, 5, TaskName.PrepMeeting, "Prepare Meeting Materials in the meeting room"),
-                new(1, 1,5, TaskName.FetchDoc, "Fetch documents from Jack for photocopy"),
-                new(1, 1,5, TaskName.DeliverDoc, "Deliver photocopied documents to Tom"),
+                new(1, 1,5, TaskName.Shred, "Shred documents in the BOSS room"),
+                new(1, 1,5, TaskName.Laminate, "Laminate documents in the BOSS room"),
+                new(2, 1, 5, TaskName.PrepRefreshment, "Prepare refreshment in the meeting room"),
+                new(3, 1, 5, TaskName.PrepMeeting, "Prepare Meeting Materials in the meeting room"),
+                new(1, 1,5, TaskName.FetchDoc, "Fetch documents from Sihan for photocopy"),
+                new(1, 1,5, TaskName.DeliverDoc, "Deliver photocopied documents back to Sihan"),
                 new(1, 1,5, TaskName.RefillCoffee, "Refill the empty coffee pot"),
             }
 
