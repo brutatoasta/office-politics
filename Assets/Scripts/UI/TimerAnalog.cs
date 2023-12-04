0299a24625c690b19857e952cd55da328f58bbc1
j-ntw <60847718+j-ntw@users.noreply.github.com>
2023-12-04 14:02:34 +0800

    add blinking to timer analog remove timer digital for level 1,2,3, map.unity

--- Assets/Scripts/UI/TimerAnalog.cs
+++ Assets/Scripts/UI/TimerAnalog.cs
@@ -1,27 +1,31 @@
-using System;
+using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 
 public class TimerAnalog : MonoBehaviour
 {
     #region Variables
-
-    private bool _isRunning = false;
+    enum TimerType { Countdown, Stopwatch }
+    [SerializeField] private TimerType timerType;
+    [SerializeField] private bool _isRunning = false;
     const float hoursInDay = 24,
     minutesInHour = 60,
-    workHours = 5 + 12 - 8,
+    workHours = 5 + 12 - 8, // 9h workday
     startHourOffset = 8f,
-    workdayDuration = 120f,
+    workdayDuration = 20f, // TODO: use GameSO or RunSO
     // day starts at 8 am, ends at 5pm
     // 9 hour workday
     hoursToDegrees = 360 / 12,
     minutesToDegrees = 360 / 60;
 
-    float elapsedRealSeconds, elapsedInGameHours, currentHour, currentMinutes = 0f;
+    float elapsedInGameHours, currentHour, currentMinutes = 0f;
+    public float elapsedRealSeconds = 0f;
     public RectTransform minuteHand;
     public RectTransform hourHand;
-    // public Image background;
+    public Image Circle;
+    public Color currentColor;
 
     #endregion
 
@@ -29,23 +33,38 @@ public class TimerAnalog : MonoBehaviour
     {
         GameManager.instance.TimerStart.AddListener(OnTimerStart);
         GameManager.instance.TimerStop.AddListener(OnTimerStop);
-        // GameManager.instance.TimerUpdate.AddListener(OnTimerUpdate);
+        GameManager.instance.TimerUpdate.AddListener(OnTimerUpdate);
+        Circle = transform.GetChild(0).GetComponent<Image>();
         minuteHand = transform.GetChild(1).GetComponent<RectTransform>();
         hourHand = transform.GetChild(2).GetComponent<RectTransform>();
+        currentColor = Circle.color;
     }
 
     private void OnTimerStart() => _isRunning = true;
     private void OnTimerStop() => _isRunning = false;
-    // private void OnTimerUpdate(float value) => timeToDisplay = value;
+    private void OnTimerRestart()
+    {
+        elapsedRealSeconds = 0f;
+        currentColor = Color.white;
+    }
+    private void SetTimerType(TimerType type) => timerType = type;
+    private void OnTimerUpdate(float value) => elapsedRealSeconds = value;
     private Color ChangeColor(float value)
     {
+
         return value switch
         {
-            float val when val / workdayDuration >= 2f / 3f => Color.red,
-            float val when val / workdayDuration >= 1f / 3f => Color.yellow,
-            float val when val / workdayDuration < 1f / 3f => Color.green,
-            _ => Color.black,
+            float val when CloseEnough(val / workdayDuration, 2f / 3f, 0.01) => timerType == TimerType.Stopwatch ? Color.red : Color.yellow,
+            float val when CloseEnough(val / workdayDuration, 1f / 3f, 0.01) => timerType == TimerType.Stopwatch ? Color.yellow : Color.red,
+            _ => currentColor,
         };
+
+
+
+    }
+    static bool CloseEnough(double value1, double value2, double acceptableDifference)
+    {
+        return Math.Abs(value1 - value2) <= acceptableDifference;
     }
     private void Update()
     {
@@ -56,7 +75,7 @@ public class TimerAnalog : MonoBehaviour
         else
         {
             // calculate time
-            elapsedRealSeconds += Time.deltaTime;
+            elapsedRealSeconds += timerType == TimerType.Countdown ? -Time.deltaTime : Time.deltaTime;
             elapsedInGameHours = elapsedRealSeconds * workHours / workdayDuration;
 
             currentHour = (startHourOffset + elapsedInGameHours) % 12;
@@ -70,10 +89,20 @@ public class TimerAnalog : MonoBehaviour
         {
             GameManager.instance.OnStopTimer();
         }
-        if (elapsedRealSeconds >= workdayDuration)
+        if (currentColor != ChangeColor(elapsedRealSeconds))
         {
-            GameManager.instance.OnStopTimer();
+            currentColor = ChangeColor(elapsedRealSeconds);
+            StartCoroutine(TimerShader(currentColor));
         }
-        // background.color = ChangeColor(elapsedRealSeconds);
+    }
+    IEnumerator TimerShader(Color color)
+    {
+        // blink 5 times
+        for (int i = 0; i < 10; i++)
+        {
+            Circle.color = (i % 2 == 0) ? color : Color.white;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+
     }
 }
