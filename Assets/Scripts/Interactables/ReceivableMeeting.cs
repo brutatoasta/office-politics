0c2a64a64f7440b872fe5a08a3944fdc09af9b6c
j-ntw <60847718+j-ntw@users.noreply.github.com>
2023-12-05 00:10:34 +0800

    fix: shader in map unity

--- Assets/Scripts/Interactables/ReceivableMeeting.cs
+++ Assets/Scripts/Interactables/ReceivableMeeting.cs
@@ -1,171 +1,125 @@
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
 
-// // Represents an interactable object that can receive items from players.
-// // Colleagues in Meeting room 
-// public class ReceivableMeeting : BaseInteractable
-// {
-//     // shredder can accept both valid and invalid inputs
-//     public TaskName[] invalidInputs;
-//     public TaskName[] validInputs;
-//     public GameObject taskIcon;
-//     public SpriteRenderer sprite;
-//     private HashSet<TaskName> _invalidInputs;
-//     private HashSet<TaskName> _validInputs; // hashset for faster checks
-//     TaskName heldType;
-//     GameObject held;
-//     public SpriteRenderer tableSprite;
-//     new void Awake()
-//     {
-//         base.Awake();
+// Represents an interactable object that can receive items from players.
+// Colleague who receive 
+public class ReceivableMeeting : BaseInteractable
+{
+    // shredder can accept both valid and invalid inputs
+    public TaskName[] invalidInputs;
+    public TaskName[] validInputs;
 
-//         _validInputs = new HashSet<TaskName>(validInputs); // TODO: no need multiple inputs
-//         _invalidInputs = new HashSet<TaskName>(invalidInputs);
-//         if (taskIcon != null)
-//         {
-//             taskIcon.SetActive(true);
-//         }
+    public SpriteRenderer tableSprite;
+    private HashSet<TaskName> _invalidInputs;
+    private HashSet<TaskName> _validInputs; // hashset for faster checks
+    TaskName heldType;
+    GameObject held;
 
-//     }
-//     protected override bool CanInteract()
-//     {
-//         held = GameManager.instance.held;
+    new void Awake()
+    {
+        base.Awake();
 
-//         if (held == null) // nothing in hand
-//         {
-//             return false;
-//         }
-//         else
-//         {
-//             if (held.GetComponent<Holdable>() == null)
-//             {
-//                 heldType = held.GetComponent<CoffeePot>().taskName;
-//             }
-//             else
-//             {
-//                 heldType = held.GetComponent<Holdable>().taskName;
-//             }
-//             return _validInputs.Contains(heldType) || _invalidInputs.Contains(heldType);
-//         }
+        _validInputs = new HashSet<TaskName>(validInputs); // TODO: no need multiple inputs
+        _invalidInputs = new HashSet<TaskName>(invalidInputs);
+        if (bubbleObj != null)
+        {
+            bubbleObj.SetActive(true);
+        }
+        tableSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        tableSprite.enabled = false; // hide it first
+    }
+    protected override bool CanInteract()
+    {
+        held = GameManager.instance.held;
 
-//     }
+        if (held == null) // nothing in hand
+        {
+            return false;
+        }
+        else
+        {
+            heldType = held.GetComponent<Holdable>().taskName;
+            return _validInputs.Contains(heldType) || _invalidInputs.Contains(heldType);
+        }
 
-//     protected override void OnInteract()
-//     {
-//         // called when player presses interact key
-//         SpriteRenderer sprite = GetComponent<SpriteRenderer>(); // TODO: what for?
+    }
+
+    protected override void OnInteract()
+    {
+        // called when player presses interact key
+
+        if (GameManager.instance.held != null)
+        {
+            // drop the item in!
+            GameManager.instance.SetHeld(null);
+            playerHand.sprite = null;
+            playerHand.color = Color.white;
+
+            // calculate score and tasks
+            if (isValidInput(heldType)) // not just holdable class, but specfically accept toShred and toLaminate types 
+            {
+                // check which task and play the sfx accordingly - Natthan
+                GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.serveCoffee);
+
+                // decrease task count
+                GameManager.instance.levelVariables.Succeed(heldType);
+                GameManager.instance.showPerformancePoint.Invoke();
+                GameManager.instance.DecreaseQuota();
+
+                // show sprite on table
+                tableSprite.enabled = true;
+
+                Debug.Log("increase score");
+
+                // disable the bubble notif
+                if (bubbleObj != null)
+                {
+                    bubbleObj.SetActive(false);
+                }
+            }
+            else
+            {
+                // decrease score
+                GameManager.instance.levelVariables.Fail(heldType);
+                GameManager.instance.showPerformancePoint.Invoke();
+                Debug.Log("decrease score");
 
 
-//         if (GameManager.instance.held != null)
-//         {
-//             // maybe check other conditions
-//             // drop the item in!
-//             GameManager.instance.SetHeld(null);
-//             playerHand.sprite = null;
-//             playerHand.color = Color.white;
+                // Play "interactionInvalid" sound - Natthan
+                // Todo: can add more specific sounds in the future
+                GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.interactionInvalid);
+            }
 
-//             //Debug.Log($"Dropped {held.name} into me!");
+        }
+    }
 
-//             // calculate score and tasks
-//             if (isValidInput(heldType)) // not just holdable class, but specfically accept toShred and toLaminate types 
-//             {
-//                 // check which task and play the sfx accordingly - Natthan
-//                 if (heldType == TaskName.FetchCoffee)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.serveCoffee);
-//                 }
-//                 else if (heldType == TaskName.RefillCoffee)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.serveCoffee);
-//                 }
-//                 else if (heldType == TaskName.FetchTea)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.serveCoffee);
-//                 }
-//                 else if (heldType == TaskName.PrepRefreshment)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.serveCoffee);
-//                 }
-//                 else if (heldType == TaskName.Shred)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.shredDocument);
-//                 }
-//                 else if (heldType == TaskName.Laminate)
-//                 {
-//                     GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.laminateDocument);
-//                 }
+    bool isValidInput(TaskName input)
+    {
+        return _validInputs.Contains(input);
+    }
 
-//                 // decrease task count
-//                 GameManager.instance.levelVariables.Succeed(heldType);
-//                 GameManager.instance.showPerformancePoint.Invoke();
-//                 GameManager.instance.DecreaseQuota();
-//                 if (sprite != null)
-//                 {
-//                     sprite.enabled = true;
-//                 }
-//                 Debug.Log("increase score");
-//                 if (taskIcon != null)
-//                 {
-//                     taskIcon.SetActive(false);
-//                 }
+    bool allAreValidInput(TaskName[] inputs)
+    {
+        foreach (TaskName value in inputs)
+        {
+            if (!_validInputs.Contains(value))
+            {
+                return false;
+            }
 
-//                 // TODO: if machine and not a person
-//                 if (animator != null)
-//                 {
-//                     animator.SetTrigger("doWiggle");
-//                 }
-//                 // putting papers/refreshment has no fail condition, but maybe put this in another method/switch statement based on validInputs
-//                 if (anyAreValidInput(new[] { TaskName.PrepMeeting, TaskName.PrepRefreshment }))
-//                 {
-//                     sprite.enabled = true;
-//                 }
-//             }
-//             else
-//             {
-//                 // decrease score
-//                 GameManager.instance.levelVariables.Fail(heldType);
-//                 GameManager.instance.showPerformancePoint.Invoke();
-//                 Debug.Log("decrease score");
-//                 if (animator != null)
-//                 {
-//                     animator.SetTrigger("doFlinch");
-//                 }
-
-//                 // Play "interactionInvalid" sound - Natthan
-//                 // Todo: can add more specific sounds in the future
-//                 GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.interactionInvalid);
-//             }
-
-//         }
-//     }
-
-//     bool isValidInput(TaskName input)
-//     {
-//         return _validInputs.Contains(input);
-//     }
-
-//     bool allAreValidInput(TaskName[] inputs)
-//     {
-//         foreach (TaskName value in inputs)
-//         {
-//             if (!_validInputs.Contains(value))
-//             {
-//                 return false;
-//             }
-
-//         }
-//         return true;
-//     }
-//     bool anyAreValidInput(TaskName[] inputs)
-//     {
-//         foreach (TaskName value in inputs)
-//         {
-//             if (_validInputs.Contains(value))
-//             {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }
+        }
+        return true;
+    }
+    bool anyAreValidInput(TaskName[] inputs)
+    {
+        foreach (TaskName value in inputs)
+        {
+            if (_validInputs.Contains(value))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
