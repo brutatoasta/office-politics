579dc132a156031d5aaf569ec3304f2e3e26ce6b
jolthl <81601016+jolthl@users.noreply.github.com>
2023-12-06 04:35:37 +0800

    added achievements, credits, fix cutscene

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -9,6 +9,8 @@ public class MainMenuUI : MonoBehaviour
     public GameObject mapPanel;
     public GameObject shade;
     public GameObject notifsButton;
+    public GameObject AchievementsPage;
+    public GameObject creditsPage;
     public void PlayGame()
     {
         GameManager.instance.LevelStart();
@@ -71,6 +73,47 @@ public class MainMenuUI : MonoBehaviour
         mapPanel.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo().setOnComplete(() =>
         {
             mapPanel.SetActive(false);
+        });
+        GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuBack);
+    }
+    public void OpenAchievements()
+    {
+        // Ensure the panel is initially at scale zero
+        AchievementsPage.transform.localScale = Vector3.zero;
+
+        // Activate the panel
+        AchievementsPage.SetActive(true);
+
+        // Start the opening animation
+        AchievementsPage.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
+        GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuClick);
+    }
+    public void CloseAchievements()
+    {
+        AchievementsPage.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo().setOnComplete(() =>
+        {
+            AchievementsPage.SetActive(false);
+        });
+        GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuBack);
+    }
+
+    public void OpenCredits()
+    {
+        // Ensure the panel is initially at scale zero
+        creditsPage.transform.localScale = Vector3.zero;
+
+        // Activate the panel
+        creditsPage.SetActive(true);
+
+        // Start the opening animation
+        creditsPage.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
+        GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuClick);
+    }
+    public void CloseCredit()
+    {
+        creditsPage.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo().setOnComplete(() =>
+        {
+            creditsPage.SetActive(false);
         });
         GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuBack);
     }
