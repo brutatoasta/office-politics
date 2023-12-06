714335dbe0689e3a327e6c16abe9a5b4d9e328b5
eharnlee <eharnlee@gmail.com>
2023-12-06 08:45:57 +0800

    Add sfx bad ending button click

--- Assets/Scripts/UI/MainMenuButton.cs
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -3,6 +3,9 @@ public class MainMenuButton : MonoBehaviour
 {
     public void MainMenu()
     {
+        // sfx for main menu button click
+        GameManager.instance.PlayAudioElement(GameManager.instance.audioElements.menuClick);
+
         Time.timeScale = 1;
         GameManager.instance.levelVariables.currentLevelIndex = 0;
         GameManager.instance.GameRestart();
